912c9403350d8e23e0128d4f5f8d2631887398d5
aabha-s <aabha@thoughtwin.com>
2022-10-31 16:15:09 +0530

    Second commit

--- controllers/addproduct.js
+++ controllers/addproduct.js
@@ -1,22 +1,40 @@
 const productsSchema = require("../models/products");
+const cloudinary = require("../cloudinary");
 
-exports.addProducts = async (req, res) => {
-  productsSchema
-    .create({
-      p_title: req.body.p_title,
-      p_image: "",
-      p_description: req.body.p_description,
-      p_rating: req.body.p_rating,
-      freshness: req.body.freshness,
-      farm: req.body.farm,
-      delivery: req.body.delivery,
-      stock: req.body.stock,
-      p_price: req.body.p_price,
-    })
-    .then((result) => {
-      return res.status(200).json(result);
-    })
-    .catch((error) => {
-      return res.status(500).json(error);
-    });
+exports.addProducts = async (request, response) => {
+  if (request.file) {
+    await cloudinary.v2.uploader
+      .upload(request.file.path)
+      .then((result) => {
+        saveProduct(request, response, result.url)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    saveProduct(request, response);
+  }
 };
+
+function saveProduct(request, response, p_image) {
+  const products = new productsSchema();
+  products.p_title = request.body.p_title;
+  products.p_image = p_image ? p_image : '';
+  products.p_description = request.body.p_description;
+  products.p_rating = request.body.p_rating;
+  products.freshness = request.body.freshness;
+  products.farm = request.body.farm;
+  products.delivery = request.body.delivery;
+  products.stock = request.body.stock;
+  products.p_price = request.body.p_price;
+  products.save()
+    .then((result) => {
+      return response.status(201).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      return response
+        .status(500)
+        .json({ message: "Oops!Something Went Wrong" });
+    });
+}
--- routes/route.js
+++ routes/route.js
@@ -25,29 +25,25 @@ const upload = multer({
   storage: storage,
 });
 
-router.post(
-  "/add_product",
-  upload.single("p_image"),
-  addController.addProducts
-);
+router.post("/add_product", uploader.single("p_image"), addController.addProducts);
 router.get("/products", getController.getProducts);
 router.get("/product/:id", productController.particularProduct);
 router.patch("/update-product/:id", updateController.updateProduct);
 router.delete("/delete-product/:id", deleteController.deleteProduct);
-router.post("/upload", upload.single("profile"), (req, res) => {
-  console.log(req.file);
-});
-
-router.post("/uploadimg", uploader.single("file"), async (req, res) => {
-  const upload = await cloudinary.v2.uploader.upload(req.file.path);
-  return res.json({
-    success: true,
-    file: upload.secure_url,
-  });
-});
-
-// process.on("unhandledRejection", (err) => {
-//   throw err;
+// router.post("/upload", upload.single("profile"), (req, res) => {
+//   console.log(req.file);
 // });
+
+// router.post("/uploadimg", uploader.single("file"), async (req, res) => {
+//   const upload = await cloudinary.v2.uploader.upload(req.file.path);
+//   return res.json({
+//     success: true,
+//     file: upload.secure_url,
+//   });
+// });
+
+process.on("unhandledRejection", (err) => {
+  throw err;
+});
 
 module.exports = router;
