7d049426ce87c8eb67d27bfb9d10e6b0272feee7
aabha-s <aabha@thoughtwin.com>
2022-11-01 18:02:14 +0530

    fixxx

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 const port = process.env.PORT || 4000;
-mongoose.connect("mongodb://0.0.0.0:27017/productdb", {
+mongoose.connect("mongodb://localhost:27017/productdb", {
   useNewUrlParser: true,
 });
 
