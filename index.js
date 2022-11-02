41deafc3c5c42e418546e8c08f0ee03b03f7ae1a
aabha-s <aabha@thoughtwin.com>
2022-11-02 12:55:23 +0530

    fixx

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/route");
+require("dotenv").config();
 const app = express();
 var cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use(router);
 const port = process.env.PORT || 4000;
-mongoose.connect("mongodb://localhost:27017/productdb", {
+// ATLAS_URL =
+//   "mongodb+srv://aabha:<password>@cluster0.lra7kvj.mongodb.net/?retryWrites=true&w=majority";
+const url =
+  "mongodb+srv://aabha:Atlas00000@cluster0.lra7kvj.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(url, {
   useNewUrlParser: true,
 });
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
+const connection = mongoose.connection;
+// connection.on("error", console.error.bind(console, "connection error: "));
+connection.once("open", function () {
   console.log("Connected successfully");
 });
 
